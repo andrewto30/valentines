050e63ec08494fd991b7f9c32a0b804adca8b4e1
andrewto30 <andrew.to30@gmail.com>
2024-02-08 15:26:04 -0500

    added more events

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ document.getElementById('yesButton').addEventListener('click', function() {
 document.getElementById('noButton').addEventListener('click', function() {
     var yesButton = document.getElementById('yesButton');
     yesButton.style.transform = 'scale(1.1)'; // Enlarge the "Yes" button
-    yesButton.innerHTML = 'Are you sure?'; // Change button text
+    yesButton.innerHTML = 'Yes Yes'; // Change button text
     document.getElementById('noButton').innerHTML = 'No, really'; // Change "No" button text
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    var yesButton = document.getElementById('yesButton');
+    yesButton.style.transform = 'scale(1.1)'; // Enlarge the "Yes" button
+    yesButton.innerHTML = 'Yes Yes Yes'; // Change button text
+    document.getElementById('noButton').innerHTML = 'Wrong button!!!'; // Change "No" button text
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    var yesButton = document.getElementById('yesButton');
+    yesButton.style.transform = 'scale(1.1)'; // Enlarge the "Yes" button
+    yesButton.innerHTML = 'Yes Yes Yes Yes'; // Change button text
+    document.getElementById('noButton').innerHTML = 'Uh this is awkward'; // Change "No" button text
 });
